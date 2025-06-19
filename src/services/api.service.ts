5bb08bf75efd7edcd9881486c6b5b0ca9a3a78c4
Jordan <arreyetta1jnr@gmail.com>
2025-06-19 11:16:51 +0100

    Update index title and change API URL to production endpoint

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class ApiService {
-  private apiUrl = 'http://localhost:5000';
+  private apiUrl = 'https://to-do-list-be-1.onrender.com';
 
   constructor(private http: HttpClient) { }
 
